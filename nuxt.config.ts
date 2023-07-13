1e7894c220d87d723fffd44230bb365476c84d4c
Pantakit-Joseph <mr.pantakit@gmail.com>
2023-07-13 11:53:54 +0700

    setup vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.css',
+    '@fortawesome/fontawesome-free/css/all.css',
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
 })
