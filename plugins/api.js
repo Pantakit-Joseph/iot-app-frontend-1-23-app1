13944cf5c487e377e9a60c44ec8517612d295714
Pantakit-Joseph <mr.pantakit@gmail.com>
2023-07-27 11:12:20 +0700

    .

--- plugins/api.js
+++ plugins/api.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 export default defineNuxtPlugin((nuxtApp) => {
-  // const config = useRuntimeConfig()
-  // const baseURL = config.app.baseURL
   const baseURL = useRuntimeConfig().public.apiBaseUrl
   const axiosInstance = axios.create({
     baseURL,
   })
   nuxtApp.provide('api', axiosInstance)
 })
+
+// export default defineNuxtPlugin((nuxtApp) => {
+//   const axiosInstance = axios.create()
+//   nuxtApp.provide('axios', axiosInstance)
+//   วิธีใช้ this.$axios
+// })
