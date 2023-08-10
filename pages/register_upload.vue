01d3a142ae7a8589e34d175918b3e86112ead309
Pantakit-Joseph <mr.pantakit@gmail.com>
2023-08-10 14:22:13 +0700

    .

---
+++ pages/register_upload.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-main class="ma-6" style="min-height: 300px;">
+      <v-card class="pa-3">
+        <v-file-input
+          v-model="file"
+          label="File input"
+          accept="image/*"
+          multiple
+        ></v-file-input>
+        <v-btn color="primary" @click="submit">
+          Submit
+        </v-btn>
+      </v-card>
+    </v-main>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    file: null,
+  }),
+  methods: {
+    async submit() {
+      console.log(this.file)
+      const formData = new FormData()
+      formData.append('id', 1234)
+      const res = await this.$api.post('/upload', formData)
+      const data = res.data
+      console.log(data)
+    },
+  },
+}
+</script>
--- pages/tests/upload.vue
+++ pages/tests/upload.vue
@@ -1,8 +1,22 @@
 <template>
-  <v-file-input v-model="files" label="File input" multiple></v-file-input>
-  <v-btn @click="submit">
-    Submit
-  </v-btn>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="ma-6" style="min-height: 300px;">
+      <v-card class="pa-3">
+        <v-file-input v-model="files" label="File input" multiple></v-file-input>
+        <v-btn color="primary" @click="submit">
+          Submit
+        </v-btn>
+      </v-card>
+    </v-main>
+  </v-layout>
 </template>
 
 <script>
