fe30fcf1ec98f405a20cc6c50c21a3f23be8e91f
Pantakit-Joseph <mr.pantakit@gmail.com>
2023-08-29 14:25:55 +0700

    mqtt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
+
 export default defineNuxtConfig({
   // ssr: false,
   devtools: { enabled: true },
@@ -13,6 +16,35 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:7001',
+    },
+  },
+  vite: {
+    resolve: {
+      alias: {
+        util: 'util/',
+      },
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: 'globalThis',
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            buffer: true,
+            process: true,
+          }),
+        ],
+      },
+    },
+    build: {
+      rollupOptions: {
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        plugins: [nodePolyfills()],
+      },
     },
   },
 })
---
+++ pages/dashboard.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container>
+    <v-card title="Title" subtitle="Subtitle">
+      <v-card-text>
+        <v-btn
+          color="primary"
+        >
+          Button
+        </v-btn>
+        <v-btn
+          color="secondary"
+        >
+          Button
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-progress-circular
+          :size="100"
+          :width="15"
+          :model-value="temp1"
+          color="primary"
+        >
+          {{ temp1 }}
+        </v-progress-circular>
+        <v-progress-circular
+          :size="100"
+          :width="15"
+          :model-value="temp2"
+          color="teal"
+        >
+          {{ temp2 }}
+        </v-progress-circular>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import * as mqtt from 'mqtt'
+export default {
+  setup() {
+    useHead({
+      script: [
+        '/js/mqtt.min.js',
+      ],
+    })
+  },
+  data: () => ({
+    temp1: 0,
+    temp2: 0,
+  }),
+  created() {
+    this.client = mqtt.connect('ws://broker.emqx.io:8083/mqtt')
+    this.client.on('connect', () => {
+      console.log('on client connect')
+      this.client.subscribe('Pantakit/temp1')
+      this.client.subscribe('Pantakit/temp2')
+    })
+    this.client.on('message', (topic, message) => {
+      if (topic === 'Pantakit/temp1') {
+        console.log('connect_message/temp1')
+        // message is Buffer
+        console.log('GOT:', message.toString())
+        this.msg_t = message.toString()
+        console.log('data=', this.msg_t)
+
+        // client.end()
+      }
+    })
+  },
+}
+</script>
